10190c1766795fd74010fc51b5d163b764a7d2e4
Srikanth Jessu <srikanth.jessu@gmail.com>
2014-08-06 00:56:22 +0530

    ChitMan InitialVersion
    ChitMan InitialVersion

---
+++ ChitMan/src/main/java/com/miya/chitman/controllers/ChitController.java
@@ -0,0 +1,14 @@
+package com.miya.chitman.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the Chit create, edit and delete pages.
+ */
+@Controller
+@RequestMapping("/chit")
+public class ChitController {
+	
+
+}
